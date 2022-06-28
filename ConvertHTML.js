35d601a15f881c4608a03b50bde2146a3a82c831
ACherkas12 <55573379+ACherkas12@users.noreply.github.com>
2022-06-28 22:33:12 +0300

    Update ConvertHTML.js

--- ConvertHTML.js
+++ ConvertHTML.js
@@ -11,4 +11,15 @@ function convertHTML(str) {
   return newstr;
 };
 
+console.log(convertHTML2("Dolce & Gabbana"));
+
+function convertHTML(str) {
+  let map = new Map([['&', '&amp;'], ['<', '&lt;'], ['>', '&gt;'], ['"', '&quot;'], ["'", '&apos;']]);
+  let newstr = '';
+  for (let i = 0; i < str.length; i++) {
+    map.has(str[i]) ? newstr += map.get(str[i]) : newstr += str[i];
+    }
+  return newstr;
+};
+
 console.log(convertHTML("Dolce & Gabbana"));
