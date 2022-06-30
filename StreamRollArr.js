4a299dbf229caba7dda2d1c350810fa97c361227
ACherkas12 <55573379+ACherkas12@users.noreply.github.com>
2022-06-30 12:48:11 +0300

    Create StreamRollArr.js

---
+++ StreamRollArr.js
@@ -0,0 +1,13 @@
+function steamrollArray(arr) {
+  var newarr = [];
+  for(var i = 0; i < arr.length; i++) {
+    if(Array.isArray(arr[i])) {
+        newarr = newarr.concat(steamrollArray(arr[i]));
+    } else {
+        newarr.push(arr[i]);
+    }
+  }
+  return newarr;
+}
+
+steamrollArray([1, [2], [3, [[4]]]]);
