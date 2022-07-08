edf65053a61b12c574cb82cc04393bbfcf01e742
ACherkas12 <55573379+ACherkas12@users.noreply.github.com>
2022-07-08 22:39:19 +0300

    Update sumOddFibbonachi.js

--- sumOddFibbonachi.js
+++ sumOddFibbonachi.js
@@ -15,3 +15,22 @@ function sumFibs(num) {
   return sum + 1;
   }
 sumFibs(4);
+
+function sumFibs(num) {
+  function fib(first, last) {
+    return first + last;
+  }
+  let firstValue = 0;
+  let secondValue = 1;
+  let sum = 1;
+  let newCurrent;
+  for (let i = 0; i <= num; i++) {
+    newCurrent = fib(firstValue, secondValue)
+    firstValue = secondValue;
+    secondValue = newCurrent;
+    if (newCurrent % 2 && newCurrent <= num)
+      sum += newCurrent;
+  }
+  console.log(sum)
+  return sum;
+}
